09aeca18b22770508aadc7b8dabf21a1619a32ff
Kannavial <vialia.kanna@gmail.com>
2024-09-01 20:09:53 +0700

    Update deployer.py

--- FINALPROJECT/deployer.py
+++ FINALPROJECT/deployer.py
@@ -28,39 +28,49 @@ def load_config(file_path):
         config = yaml.safe_load(file)
     return config
 
-def launch_ec2_instance(config, ec2DeployName):
-    session = boto3.Session(profile_name='deployerProfile')
-    ec2 = session.client('ec2')
+def launch_ec2_instance(config, configs_name, ec2DeployName):
+    try:
+        session = boto3.Session(profile_name='deployerProfile')
+        ec2 = session.client('ec2')
+    except Exception as e:
+        print(f"Error creating boto3 session: {e}")
 
-    ami_id = config['ec2'].get('ami_id')
-    instance_type = config['ec2'].get('instance_type')
-    key_name = config['ec2'].get('key_name')
-    security_group_id = config['ec2'].get('security_group_id')
+    ami_id = config[configs_name].get('ami_id')
+    instance_type = config[configs_name].get('instance_type')
+    key_name = config[configs_name].get('key_name')
+    security_group_id = config[configs_name].get('security_group_id')
 
-    response = ec2.run_instances(
-        ImageId=ami_id,
-        InstanceType=instance_type,
-        KeyName=key_name,
-        SecurityGroupIds=[security_group_id],
-        MinCount=1,
-        MaxCount=1,
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': f'{ec2DeployName}_ec2Instance'
-                    }
-                ]
-            }
-        ]
-    )
+    if(ami_id == "" or instance_type == "" or key_name == "" or security_group_id == ""):
+        exit("Please provide ami_id, instance_type, key_name, and security_group_id in the config file")
     
-    instance_id = response['Instances'][0]['InstanceId']
-    print(f"EC2 Instance launched with ID: {instance_id}")
+    try:
+        response = ec2.run_instances(
+            ImageId=ami_id,
+            InstanceType=instance_type,
+            KeyName=key_name,
+            SecurityGroupIds=[security_group_id],
+            MinCount=1,
+            MaxCount=1,
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': f'{ec2DeployName}_ec2Instance'
+                        }
+                    ]
+                }
+            ]
+        )
+    
+        instance_id = response['Instances'][0]['InstanceId']
+        print(f"EC2 Instance launched with ID: {instance_id}")
 
-    return response['Instances'][0]['PrivateIpAddress']
+        return response['Instances'][0]['PrivateIpAddress']
+    except Exception as e:
+        print(f"Error launching EC2 instance: {e}")
+        return None
 
 def upload_directory(local_dir, remote_dir, ssh_client, sftp_client):
     # Create remote directory if it does not exist
@@ -100,16 +110,24 @@ def connect_and_upload(instance_ip, key_path, local_dir, remote_dir):
     ssh.close()
 
 def main(repo_url, name):
-
     repo_url = sys.argv[1]
     name = sys.argv[2]
+    configs_name = sys.argv[3]
 
-    _clone_repo = clone_repo(repo_url, name)
-    zip_directory(f"{_clone_dir}/{_clone_repo}", f"{name}_files_zipped.zip")
-    configs = load_config('/Configs/Config.yaml')
-    instanceIP = launch_ec2_instance(configs, name)
-    connect_and_upload(instanceIP, configs['ec2'].get('key_name'), f"{name}_Clone", '/home/ec2-user')
-    os.rmdir(f"{_clone_dir}/{_clone_repo}") 
+    if(repo_url == "" or name == ""):
+        exit("Please provide a repo url and a name for the ec2 instance")
+    if(configs_name == ""):
+        configs_name = 'default'
+        
+    try:
+        _clone_repo = clone_repo(repo_url, name)
+        zip_directory(f"/{_clone_repo}", f"{name}_files_zipped.zip")
+        configs = load_config('/Configs/Config.yaml')
+        instanceIP = launch_ec2_instance(configs, configs_name, name)
+        connect_and_upload(instanceIP, configs['ec2'].get('key_name'), f"{name}_Clone", '/home/ec2-user')
+        os.rmdir(f"/{_clone_repo}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
